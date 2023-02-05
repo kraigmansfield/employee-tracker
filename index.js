8a88a1153b3c268a068549fa8a2b22661e4d2644
kraigmansfield <Kraig.mansfield@gmail.com>
2023-02-05 12:22:56 -0900

    updates

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const mysql = require('mysql2')
-const inquirer = require('inquirer')
-const PORT = process.env.PORT || 3001
+const mysql = require('mysql2');
+const express = require('express');
+const inquirer = require('inquirer');
+
+
+
+const PORT = process.env.PORT || 3000
 require('console.table')
+const app = express();
 
 const db = mysql.createConnection(
   {
@@ -13,6 +18,58 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the books_db database.`),
 )
+
+inquirer.prompt([
+    {
+        type:"list",
+        message:"Please select an option.",
+        name: 'choicesMain',
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add An Employee", "Update An Employee Role", "Exit"]
+    }
+]).then((res)=>{
+    console.log(res.choicesMain);
+    switch(res.choicesMain){
+        case "View All Departments":
+            //TODO: ADD function
+            break;
+        
+        case "View All Roles":
+            //TODO: ADD function
+            break;
+        
+        case "View All Employees":
+            //TODO: ADD function
+            break;
+        
+        case "Add a Department":
+            //TODO: ADD function
+            break;
+        
+        case "Add a Role":
+            //TODO: ADD function
+            break;
+        
+        case "Add An Employee":
+            //TODO: ADD function
+            break;
+        
+        case "Update An Employee Role":
+            //TODO: ADD function
+            break;
+        
+        case "Exit":
+            //TODO: ADD function
+            break;
+        
+    }
+}).catch((err)=>{
+    if(err)throw err;
+});
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
